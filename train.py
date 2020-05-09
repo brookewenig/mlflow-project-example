120a85d0d252652e3e0af239301f75442f6e86f6
Brooke Wenig <brookewenig@gmail.com>
2020-05-09 09:09:47 -0700

    3

--- train.py
+++ train.py
@@ -6,9 +6,6 @@ import mlflow
 import mlflow.spark
 import pandas as pd
 import click
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("mlflow-project").getOrCreate()
 
 # Create train/test split
 filePath = "/Users/brookewenig/LearningSparkV2/databricks-datasets/learning-spark-v2/sf-airbnb/sf-airbnb-clean.parquet"
@@ -38,29 +35,29 @@ def mlflow_rf(num_trees, max_depth):
     mlflow.log_param("num_trees", num_trees)
     mlflow.log_param("max_depth", max_depth)
 
-    rf = RandomForestRegressor(labelCol="price", maxBins=40, 
-    maxDepth=max_depth, numTrees=num_trees, seed=42)
+    # rf = RandomForestRegressor(labelCol="price", maxBins=40, 
+    # maxDepth=max_depth, numTrees=num_trees, seed=42)
 
-    pipeline = Pipeline(stages=[stringIndexer, vecAssembler, rf])
+    # pipeline = Pipeline(stages=[stringIndexer, vecAssembler, rf])
 
-    # Log model
-    pipelineModel = pipeline.fit(trainDF)
-    mlflow.spark.log_model(pipelineModel, "model")
+    # # Log model
+    # pipelineModel = pipeline.fit(trainDF)
+    # mlflow.spark.log_model(pipelineModel, "model")
 
-    # Log metrics: RMSE and R2
-    predDF = pipelineModel.transform(testDF)
-    regressionEvaluator = RegressionEvaluator(predictionCol="prediction", 
-                                            labelCol="price")
-    rmse = regressionEvaluator.setMetricName("rmse").evaluate(predDF)
-    r2 = regressionEvaluator.setMetricName("r2").evaluate(predDF)
-    mlflow.log_metrics({"rmse": rmse, "r2": r2})
+    # # Log metrics: RMSE and R2
+    # predDF = pipelineModel.transform(testDF)
+    # regressionEvaluator = RegressionEvaluator(predictionCol="prediction", 
+    #                                         labelCol="price")
+    # rmse = regressionEvaluator.setMetricName("rmse").evaluate(predDF)
+    # r2 = regressionEvaluator.setMetricName("r2").evaluate(predDF)
+    # mlflow.log_metrics({"rmse": rmse, "r2": r2})
 
-    # Log artifact: Feature Importance Scores
-    rfModel = pipelineModel.stages[-1]
-    pandasDF = (pd.DataFrame(list(zip(vecAssembler.getInputCols(), 
-                                    rfModel.featureImportances)), 
-                          columns=["feature", "importance"])
-              .sort_values(by="importance", ascending=False))
-    # First write to local filesystem, then tell MLflow where to find that file
-    pandasDF.to_csv("feature-importance.csv", index=False)
-    mlflow.log_artifact("feature-importance.csv")
+    # # Log artifact: Feature Importance Scores
+    # rfModel = pipelineModel.stages[-1]
+    # pandasDF = (pd.DataFrame(list(zip(vecAssembler.getInputCols(), 
+    #                                 rfModel.featureImportances)), 
+    #                       columns=["feature", "importance"])
+    #           .sort_values(by="importance", ascending=False))
+    # # First write to local filesystem, then tell MLflow where to find that file
+    # pandasDF.to_csv("feature-importance.csv", index=False)
+    # mlflow.log_artifact("feature-importance.csv")
