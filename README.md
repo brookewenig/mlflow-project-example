3985daeb8bdc9db95943176f69f9471b2e08291f
Brooke Wenig <brookewenig@gmail.com>
2020-05-09 15:23:09 -0700

    works for spark 2.4

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# mlflow-project-example (WIP)
+# mlflow-project-example
+
+NOTE: This code needs to be updated for Spark 3.0!
 
 To run this code locally, you can clone the repo and execute:
-* mlflow run . -P file_path=/data/sf-airbnb-clean.parquet 
+* mlflow run . -P file_path=data/sf-airbnb-clean.parquet -P max_depth=5 -P num_trees=100
 
 Or, you can run it this way:
 * mlflow.run("https://github.com/brookewenig/mlflow-project-example", parameters={"max_depth": 5, "num_trees": 100})
--- train.py
+++ train.py
@@ -1,5 +1,10 @@
 from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.evaluation import RegressionEvaluator
 import mlflow
+import mlflow.spark
 import pandas as pd
 import click
 
@@ -12,15 +17,48 @@ spark = SparkSession.builder.appName("App").getOrCreate()
 def mlflow_rf(file_path, num_trees, max_depth):
   with mlflow.start_run(run_name="random-forest") as run:
     # Create train/test split
+    spark = SparkSession.builder.appName("App").getOrCreate()
     airbnbDF = spark.read.parquet(file_path)
     (trainDF, testDF) = airbnbDF.randomSplit([.8, .2], seed=42)
-    
-    # Log params: Num Trees and Max Depth  
+
+    # Prepare the StringIndexer and VectorAssembler
+    numericCols = [field for (field, dataType) in trainDF.dtypes
+                   if ((dataType == "double") & (field != "price"))]
+    vecAssembler = VectorAssembler(inputCols=numericCols,
+                                   outputCol="features")
+    # Log params: Num Trees and Max Depth
     mlflow.log_param("num_trees", num_trees)
     mlflow.log_param("max_depth", max_depth)
-    
-    # Log metric
-    mlflow.log_metric("count", trainDF.count())
+
+    rf = RandomForestRegressor(labelCol="price",
+                               maxBins=40,
+                               maxDepth=max_depth,
+                               numTrees=num_trees,
+                               seed=42)
+
+    pipeline = Pipeline(stages=[vecAssembler, rf])
+
+    # Log model
+    pipelineModel = pipeline.fit(trainDF)
+    mlflow.spark.log_model(pipelineModel, "model")
+
+    # Log metrics: RMSE and R2
+    predDF = pipelineModel.transform(testDF)
+    regressionEvaluator = RegressionEvaluator(predictionCol="prediction",
+                                            labelCol="price")
+    rmse = regressionEvaluator.setMetricName("rmse").evaluate(predDF)
+    r2 = regressionEvaluator.setMetricName("r2").evaluate(predDF)
+    mlflow.log_metrics({"rmse": rmse, "r2": r2})
+
+    # Log artifact: Feature Importance Scores
+    rfModel = pipelineModel.stages[-1]
+    pandasDF = (pd.DataFrame(list(zip(vecAssembler.getInputCols(),
+                                    rfModel.featureImportances)),
+                          columns=["feature", "importance"])
+              .sort_values(by="importance", ascending=False))
+    # First write to local filesystem, then tell MLflow where to find that file
+    pandasDF.to_csv("/tmp/feature-importance.csv", index=False)
+    mlflow.log_artifact("/tmp/feature-importance.csv")
 
 if __name__ == "__main__":
   mlflow_rf()
